28a39045615e0c230cc7608c842f7004c640335c
Vinnie marone <vinnie.marone35@gmail.com>
2021-05-25 22:48:29 -0400

    Remove auth stuff

--- src/store/store.js
+++ src/store/store.js
@@ -4,29 +4,4 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-	state: {
-		logged_in: false
-	},
-	mutations: {
-		authLoggedInState: function (state, loggedInState)
-		{
-			state.logged_in = loggedInState
-		}
-	},
-	actions: {
-		loggedIn: function ({ commit }, loggedInState)
-		{
-			if (loggedInState !== false && loggedInState !== true)
-			{
-				throw new Error('Invalid Log In State')
-			}
-			commit('authLoggedInState', loggedInState)
-		}
-	},
-	getters: {
-		isLoggedIn: state =>
-		{
-			return state.logged_in
-		}
-	}
 })
