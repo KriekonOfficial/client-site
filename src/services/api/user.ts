4607de8a1dcc6bd99cd2c40763a42d7d5f34c5cc
Vinnie marone <vinnie.marone35@gmail.com>
2021-06-20 01:54:37 -0400

    Some user client work

--- src/services/api/user.ts
+++ src/services/api/user.ts
@@ -1,16 +1,19 @@
+import { Client } from '../client'
+import { Model } from '../model'
 
-export class UserModel {
-	USERID: number | null;
-	first_name: string | null;
-	last_name: string | null;
-	email: string | null;
-	username: string | null;
-	date_of_birth: string | null;
-	registration_time: string | null;
-	verified: number | null;
-	locked: number | null;
+export class UserModel extends Model {
+	private USERID: number | null;
+	private first_name: string | null;
+	private last_name: string | null;
+	private email: string | null;
+	private username: string | null;
+	private date_of_birth: string | null;
+	private registration_time: string | null;
+	private verified: number | null;
+	private locked: number | null;
 
 	constructor() {
+		super();
 		this.USERID = null;
 		this.first_name = null;
 		this.last_name = null;
@@ -21,10 +24,86 @@ export class UserModel {
 		this.verified = null;
 		this.locked = null;
 	}
+
+	public setUserID(USERID: number) {
+		this.USERID = USERID;
+	}
+
+	public setFirstName(first_name: string) {
+		this.first_name = first_name;
+	}
+
+	public setLastName(last_name: string) {
+		this.last_name = last_name;
+	}
+
+	public setEmail(email: string) {
+		this.email = email;
+	}
+
+	public setUsername(username: string) {
+		this.username = username;
+	}
+
+	public setDateOfBirth(date_of_birth: string) {
+		this.date_of_birth = date_of_birth;
+	}
+
+	public setRegistrationTime(registration_time: string) {
+		this.registration_time = registration_time;
+	}
+
+	public setVerified(verified: number) {
+		this.verified = verified;
+	}
+
+	public setLocked(locked: number) {
+		this.locked = locked;
+	}
 }
 
-export default class UserClient {
-	constructor() {
-		//
+export class UserClient extends Client {
+	login(email:string, password:string) : Promise<UserModel|Error> {
+		return new Promise((resolve, reject) => {
+			this.post('user/login', [], {'email': email, 'password': password})
+			.then((response) => {
+				this.info()
+				.then((model) => {
+					return resolve(model);
+				})
+				.catch(() => {
+					return reject(new UserModel());
+				});
+			})
+			.catch((error) => {
+				return reject(new Error(error.response.data.meta.message));
+			});
+		});
+	}
+
+	info() : Promise<UserModel|Error> {
+		return new Promise((resolve, reject) => {
+			this.get('user')
+			.then((response) => {
+				let user = new UserModel();
+				let attr = response.data.data;
+				user.setUserID(attr.USERID);
+				user.setFirstName(attr.first_name);
+				user.setLastName(attr.last_name);
+				user.setLocked(attr.locked);
+				user.setVerified(attr.verified);
+				user.setUsername(attr.username);
+				user.setEmail(attr.email);
+				user.setDateOfBirth(attr.date_of_birth);
+				user.setRegistrationTime(attr.registration_time);
+
+				user.setInitialized(true);
+
+				return resolve(user);
+			})
+			.catch((error) => {
+				return reject(new Error(error.response.data.meta.message));
+			});
+		});
 	}
 }
--- src/services/client.ts
+++ src/services/client.ts
@@ -1,8 +1,52 @@
-import UserClient from './api/user'
-export class Client {
-	user: UserClient;
+import axios, { AxiosInstance } from 'axios';
 
-	constructor(config: string[] = []) {
-		this.user = new UserClient();
+export class Client {
+	protected static instance: AxiosInstance | null = null;
+
+	protected version: number = 1;
+
+	public constructor(config: string[] = []) {
+		if (Client.instance === null) {
+			Client.instance = axios.create({
+				timeout: 20000,
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				withCredentials: true
+			});
+		}
+	}
+	protected isInitialized() : boolean {
+		return Client.instance !== null;
+	}
+
+	protected async get(uri: string, params: any[] = [], additional_params: object = {}) : Promise<any> {
+		if (!this.isInitialized()) {
+			return Promise.reject('Client is not initialized');
+		}
+
+		return Client.instance!.get(this.generateURL(uri, params), additional_params);
+	}
+
+	protected async post(uri: string, params: any[] = [], post_params: object = {}) : Promise<any> {
+		if (!this.isInitialized()) {
+			return Promise.reject('Client is not initialized');
+		}
+
+		return Client.instance!.post(this.generateURL(uri, params), post_params)
+	}
+
+	private generateURL(uri: string, params: any[] = []) : string {
+		let url = 'https://api.kriekon.com';
+		if (window.location.host === 'local.www.kriekon.com') {
+			url = 'http://local.api.kriekon.com';
+		}
+		url += '/v' + this.version + '/' + uri;
+
+		if (params.length >= 1) {
+			url += '/' + params.join('/');
+		}
+
+		return url;
 	}
 }
