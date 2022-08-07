f65da5bf8adfcf2f3da509f0eea51917f6c4a1de
Porthorian <vinnie.marone35@gmail.com>
2022-08-07 05:56:11 -0400

    Temporary fix to webpack not loading VideoBackground

--- src/components/coming_soon.vue
+++ src/components/coming_soon.vue
@@ -1,46 +1,54 @@
 <template>
 	<div id="coming_soon">
-		<video-background class="videobg"
-		:src="require(`@/assets/cthulhu_front.mp4`)"
-		:poster="require(`@/assets/cthulhu_front.png`)"
-		>
-			<div class="container" style="height:100%">
+		<div class="container" style="height:100%">
+			<div class="poster-image" :style="{ backgroundImage: 'url(' + require('@/assets/cthulhu_front.png') + ')' }">
 				<div class="center-contents">
 					<h1 class="margin-20 center-text">Coming Soon</h1>
 					<countdownclock date="2022-12-24 08:15:00"></countdownclock>
 				</div>
 			</div>
-		</video-background>
+		</div>
 	</div>
 </template>
 
 <style scoped>
 .center-contents {
 	position: absolute;
-    top: 37%;
+    top: 42%;
     left: 50%;
     margin-right: -50%;
     transform: translate(-50%, -50%);
     margin: 0;
 }
 #coming_soon {
-	height:  100vh;
+	height:  93vh;
 }
 .videobg {
 	width: 100%;
-	height: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
 }
 .center-text {
 	text-align: center;
+}
+.poster-image {
+	width:  100%;
+	overflow: hidden;
+	background-size:  cover;
+	background-position: center;
+	height: 100%;
+	max-height: 100vh;
+	top: 0;
+	left: 0;
+	position: absolute;
 }
 </style>
 
 <script>
 import countdownclock from '@/components/countdown_clock.vue'
-import VideoBackground from 'vue-responsive-video-background-player'
 export default {
 	components: {
-		VideoBackground,
 		countdownclock
 	}
 }
